04c9da3e02fb5bafea04b07ccfa9adc57a3e8372
Eric Newcomer <ericnewcomer@gmail.com>
2017-07-19 11:48:49 -0500

    restrict special case to only space delimiters

--- excellent/functions.go
+++ excellent/functions.go
@@ -912,11 +912,13 @@ func WordCount(env utils.Environment, args ...interface{}) interface{} {
 }
 
 // Field splits `string` based on the passed in `delimiter` and returns the field at `offset`.  When splitting
-// with spaces, the delimiter is considered to be all whitespace.  (first field is 0)
+// with a space, the delimiter is considered to be all whitespace.  (first field is 0)
 //
 //   @(field("a,b,c", 1, ",")) -> "b"
 //   @(field("a,,b,c", 1, ",")) -> ""
 //   @(field("a   b c", 1, " ")) -> "b"
+//   @(field("a		b	c	d", 1, "	")) -> ""
+//   @(field("a\t\tb\tc\td", 1, " ")) -> ""
 //   @(field("a,b,c", "foo", ",")) -> ERROR
 //
 // @function field(string, offset, delimeter)
@@ -945,8 +947,8 @@ func Field(env utils.Environment, args ...interface{}) interface{} {
 		return ""
 	}
 
-	// when using spaces as a delimiter, we consider it splitting on whitespace, so remove empty values
-	if len(strings.TrimSpace(sep)) == 0 {
+	// when using a space as a delimiter, we consider it splitting on whitespace, so remove empty values
+	if sep == " " {
 		var newFields []string
 		for _, field := range fields {
 			if field != "" {
