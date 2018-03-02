de5b5740a0a58017007a98d4a3b63b33ca5736a3
Nic Pottier <nicpottier@gmail.com>
2018-03-02 16:57:19 -0500

    update test ports to 8800

--- cmd/flowserver/server_test.go
+++ cmd/flowserver/server_test.go
@@ -3,15 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/nyaruka/gocommon/urns"
-	"github.com/nyaruka/goflow/flows/events"
-	"github.com/nyaruka/goflow/flows/inputs"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/nyaruka/gocommon/urns"
+	"github.com/nyaruka/goflow/flows/events"
+	"github.com/nyaruka/goflow/flows/inputs"
 
 	"github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
@@ -272,82 +273,82 @@ func (ts *ServerTestSuite) buildResumeRequest(assetsJSON string, session flows.S
 
 func (ts *ServerTestSuite) TestHomePages() {
 	// hit our home page
-	status, body := ts.testHTTPRequest("GET", "http://localhost:8080/", "")
+	status, body := ts.testHTTPRequest("GET", "http://localhost:8800/", "")
 	ts.Equal(200, status)
 	ts.Contains(string(body), "Echo Flow")
 
 	// hit our version endpoint
-	status, body = ts.testHTTPRequest("GET", "http://localhost:8080/version", "")
+	status, body = ts.testHTTPRequest("GET", "http://localhost:8800/version", "")
 	ts.Equal(200, status)
 	ts.Contains(string(body), "version")
 }
 
 func (ts *ServerTestSuite) TestExpression() {
 	// try the expression endpoint with a valid expression
-	status, body := ts.testHTTPRequest("POST", "http://localhost:8080/expression", `{"expression": "@(1 + 2)", "context": {}}`)
+	status, body := ts.testHTTPRequest("POST", "http://localhost:8800/expression", `{"expression": "@(1 + 2)", "context": {}}`)
 	ts.Equal(200, status)
 	ts.assertExpressionResponse(body, "3", []string{})
 
 	// try the expression endpoint with an unparseable expression... which we treat as not an expression
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/expression", `{"expression": "@(1 + 2", "context": {}}`)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/expression", `{"expression": "@(1 + 2", "context": {}}`)
 	ts.Equal(200, status)
 	ts.assertExpressionResponse(body, "@(1 + 2", []string{})
 
 	// try the expression endpoint with a missing variable
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/expression", `{"expression": "@(foo + 2)", "context": {}}`)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/expression", `{"expression": "@(foo + 2)", "context": {}}`)
 	ts.Equal(200, status)
 	ts.assertExpressionResponse(body, "", []string{"no such variable: foo"})
 }
 
 func (ts *ServerTestSuite) TestFlowStartAndResume() {
 	// try to GET the start endpoint
-	status, body := ts.testHTTPRequest("GET", "http://localhost:8080/flow/start", "")
+	status, body := ts.testHTTPRequest("GET", "http://localhost:8800/flow/start", "")
 	ts.Equal(405, status)
 	ts.assertErrorResponse(body, []string{"method not allowed"})
 
 	// ry POSTing nothing to the start endpoint
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/start", "")
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/start", "")
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"unexpected end of JSON input"})
 
 	// try POSTing empty JSON to the start endpoint
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/start", "{}")
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/start", "{}")
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"field 'asset_server' is required", "field 'trigger' is required"})
 
 	// try POSTing an incomplete trigger to the start endpoint
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/start", `{"asset_server": {}, "trigger": {"type": "manual"}}`)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/start", `{"asset_server": {}, "trigger": {"type": "manual"}}`)
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"field 'flow' on 'trigger[type=manual]' is required", "field 'triggered_on' on 'trigger[type=manual]' is required"})
 
 	// try POSTing to the start endpoint a structurally invalid flow asset
 	requestBody := fmt.Sprintf(startRequestTemplate, testStructurallyInvalidFlowAssets)
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/start", requestBody)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/start", requestBody)
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"unable to read asset[url=http://testserver/assets/flow/76f0a02f-3b75-4b86-9064-e9195e1b3a02]: destination 714f1409-486e-4e8e-bb08-23e2943ef9f6 of exit[uuid=37d8813f-1402-4ad2-9cc2-e9054a96525b] isn't a known node"})
 
 	// try POSTing to the start endpoint a flow asset that references a non-existent group asset
 	requestBody = fmt.Sprintf(startRequestTemplate, testFlowMissingGroupAssets)
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/start", requestBody)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/start", requestBody)
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"validation failed for flow[uuid=76f0a02f-3b75-4b86-9064-e9195e1b3a02]: validation failed for action[uuid=ad154980-7bf7-4ab8-8728-545fd6378912, type=add_to_group]: no such group with uuid '77a1bb5c-92f7-42bc-8a54-d21c1536ebc0'"})
 
 	// POST to the start endpoint with a valid flow with no wait (it should complete)
 	requestBody = fmt.Sprintf(startRequestTemplate, testValidFlowWithNoWaitAssets)
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/start", requestBody)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/start", requestBody)
 	ts.Equal(200, status)
 
 	session, _ := ts.parseSessionResponse(ts.flowServer.assetCache, body)
 	ts.Equal(flows.SessionStatus("completed"), session.Status())
 
 	// try to resume this completed session but with no caller events
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/resume", ts.buildResumeRequest(`[]`, session, []flows.Event{}))
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/resume", ts.buildResumeRequest(`[]`, session, []flows.Event{}))
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"field 'events' must have a minimum of 1 items"})
 
 	// try to resume this completed session
 	msg := inputs.NewMsgInput(flows.InputUUID(uuid.NewV4().String()), nil, time.Now(), urns.NewTelegramURN(1234567, "bob"), "hello", nil)
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/resume", ts.buildResumeRequest(`[]`, session, []flows.Event{
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/resume", ts.buildResumeRequest(`[]`, session, []flows.Event{
 		events.NewMsgReceivedEvent(msg),
 	}))
 	ts.Equal(400, status)
@@ -355,28 +356,28 @@ func (ts *ServerTestSuite) TestFlowStartAndResume() {
 
 	// start another session on a flow that will wait for input
 	requestBody = fmt.Sprintf(startRequestTemplate, testValidFlowWithWaitAssets)
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/start", requestBody)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/start", requestBody)
 	ts.Equal(200, status)
 
 	waitingSession, _ := ts.parseSessionResponse(ts.flowServer.assetCache, body)
 	ts.Equal(flows.SessionStatus("waiting"), waitingSession.Status())
 
 	// try to resume this session with a structurally invalid version of the flow passed as an asset
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/resume", ts.buildResumeRequest(testStructurallyInvalidFlowAssets, waitingSession, []flows.Event{
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/resume", ts.buildResumeRequest(testStructurallyInvalidFlowAssets, waitingSession, []flows.Event{
 		events.NewMsgReceivedEvent(msg),
 	}))
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"unable to read asset[url=http://testserver/assets/flow/76f0a02f-3b75-4b86-9064-e9195e1b3a02]: destination 714f1409-486e-4e8e-bb08-23e2943ef9f6 of exit[uuid=37d8813f-1402-4ad2-9cc2-e9054a96525b] isn't a known node"})
 
 	// try to resume this session with a invalid version of the flow which is missing a group
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/resume", ts.buildResumeRequest(testFlowMissingGroupAssets, waitingSession, []flows.Event{
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/resume", ts.buildResumeRequest(testFlowMissingGroupAssets, waitingSession, []flows.Event{
 		events.NewMsgReceivedEvent(msg),
 	}))
 	ts.Equal(400, status)
 	ts.assertErrorResponse(body, []string{"validation failed for flow[uuid=76f0a02f-3b75-4b86-9064-e9195e1b3a02]: validation failed for action[uuid=ad154980-7bf7-4ab8-8728-545fd6378912, type=add_to_group]: no such group with uuid '77a1bb5c-92f7-42bc-8a54-d21c1536ebc0'"})
 
 	// check we can resume if we include a fixed version of the flow as an asset
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/resume", ts.buildResumeRequest(testValidFlowWithWaitAssets, waitingSession, []flows.Event{
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/resume", ts.buildResumeRequest(testValidFlowWithWaitAssets, waitingSession, []flows.Event{
 		events.NewMsgReceivedEvent(msg),
 	}))
 	ts.Equal(200, status)
@@ -392,7 +393,7 @@ func (ts *ServerTestSuite) TestFlowStartAndResume() {
 	sessionJSON = strings.Replace(sessionJSON, "a58be63b-907d-4a1a-856b-0bb5579d7507", "626daa56-2fac-48eb-825d-af9a7ab23a2c", -1)
 
 	// and try to resume that
-	status, body = ts.testHTTPRequest("POST", "http://localhost:8080/flow/resume", sessionJSON)
+	status, body = ts.testHTTPRequest("POST", "http://localhost:8800/flow/resume", sessionJSON)
 	ts.Equal(200, status)
 
 	// check we got back an errored session
