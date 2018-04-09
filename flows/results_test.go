f2de51de479ccbc68f854fc2100a78d909c94180
Rowan Seymour <rowanseymour@gmail.com>
2018-04-09 11:14:53 -0500

    Fix test (/flows/ package now fully ported)

--- flows/results_test.go
+++ flows/results_test.go
@@ -10,20 +10,22 @@ import (
 )
 
 func TestResults(t *testing.T) {
-	var resultTests = []struct {
+	var ERROR = types.NewXErrorf("any error")
+
+	var tests = []struct {
 		JSON     []byte
 		lookup   string
-		expected string
+		expected types.XValue
 	}{
-		{[]byte(`{}`), "key", ""},
-		{[]byte(`{ "name": { "result_name": "Name", "value": "Ryan Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "key", ""},
-		{[]byte(`{ "name": { "result_name": "Name", "value": "Ryan Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "name", "Ryan Lewis"},
-		{[]byte(`{ "last_name": { "result_name": "Last Name", "value": "Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "last_name", "Lewis"},
-		{[]byte(`{ "last_name": { "result_name": "Last Name", "value": "Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "Last Name", "Lewis"},
+		{[]byte(`{}`), "key", ERROR},
+		{[]byte(`{ "name": { "result_name": "Name", "value": "Ryan Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "key", ERROR},
+		{[]byte(`{ "name": { "result_name": "Name", "value": "Ryan Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "name", types.NewXString("Ryan Lewis")},
+		{[]byte(`{ "last_name": { "result_name": "Last Name", "value": "Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "last_name", types.NewXString("Lewis")},
+		{[]byte(`{ "last_name": { "result_name": "Last Name", "value": "Lewis", "node": "uuid", "created_on": "2000-01-01T00:00:00.000000000-00:00"}}`), "Last Name", types.NewXString("Lewis")},
 	}
 
 	env := utils.NewDefaultEnvironment()
-	for _, test := range resultTests {
+	for _, test := range tests {
 		results := NewResults()
 		err := json.Unmarshal(test.JSON, &results)
 		if err != nil {
@@ -32,9 +34,16 @@ func TestResults(t *testing.T) {
 		}
 		value := excellent.ResolveXValue(env, results, test.lookup)
 
-		valueStr := types.ToXString(value).Native()
-		if valueStr != test.expected {
-			t.Errorf("Expected: '%s' Got: '%s' for lookup: '%s' and Results:\n%s", test.expected, valueStr, test.lookup, test.JSON)
+		// don't check error equality - just check that we got an error if we expected one
+		errExpected, _ := test.expected.(types.XError)
+		errReturned, _ := value.(types.XError)
+		if errExpected != nil && errReturned != nil {
+			continue
+		}
+
+		cmp, _ := types.Compare(value, test.expected)
+		if cmp != 0 {
+			t.Errorf("Expected: '%s' Got: '%s' for lookup: '%s' and Results:\n%s", test.expected, value, test.lookup, test.JSON)
 		}
 	}
 }
