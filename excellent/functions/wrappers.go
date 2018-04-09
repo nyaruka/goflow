8812dea60c480e83f0596b4f1dfdf5091872b9c3
Rowan Seymour <rowanseymour@gmail.com>
2018-04-09 11:05:47 -0500

    Add ErrorPassThru function wrapper

--- excellent/functions/wrappers.go
+++ excellent/functions/wrappers.go
@@ -6,101 +6,69 @@ import (
 	"strings"
 )
 
+// ErrorPassThru wraps an XFunction and returns immediately if any arg is an error
+func ErrorPassThru(f XFunction) XFunction {
+	return func(env utils.Environment, args ...types.XValue) types.XValue {
+		for _, arg := range args {
+			if types.IsError(arg) {
+				return arg
+			}
+		}
+		return f(env, args...)
+	}
+}
+
+// ArgCountCheck wraps an XFunction and checks the number of args
+func ArgCountCheck(name string, count int, f XFunction) XFunction {
+	return func(env utils.Environment, args ...types.XValue) types.XValue {
+		if len(args) != count {
+			return types.NewXErrorf("%s takes %d argument(s), got %d", strings.ToUpper(name), count, len(args))
+		}
+		return f(env, args...)
+	}
+}
+
 // OneStringFunction creates an XFunction from a single string function
 func OneStringFunction(name string, f func(utils.Environment, types.XString) types.XValue) XFunction {
-	return func(env utils.Environment, args ...types.XValue) types.XValue {
-		if len(args) != 1 {
-			return types.NewXErrorf("%s takes a single argument, got %d", strings.ToUpper(name), len(args))
-		}
-
-		// if argument is an error, return immediately
-		if types.IsError(args[0]) {
-			return args[0]
-		}
-
+	return ErrorPassThru(ArgCountCheck(name, 1, func(env utils.Environment, args ...types.XValue) types.XValue {
 		return f(env, types.ToXString(args[0]))
-	}
+	}))
 }
 
 // TwoStringFunction creates an XFunction from a function that takes two strings
 func TwoStringFunction(name string, f func(utils.Environment, types.XString, types.XString) types.XValue) XFunction {
-	return func(env utils.Environment, args ...types.XValue) types.XValue {
-		if len(args) != 2 {
-			return types.NewXErrorf("%s takes two arguments, got %d", strings.ToUpper(name), len(args))
-		}
-
-		// if either argument is an error, return immediately
-		if types.IsError(args[0]) {
-			return args[0]
-		}
-		if types.IsError(args[1]) {
-			return args[1]
-		}
-
+	return ErrorPassThru(ArgCountCheck(name, 2, func(env utils.Environment, args ...types.XValue) types.XValue {
 		return f(env, types.ToXString(args[0]), types.ToXString(args[1]))
-	}
+	}))
 }
 
 // StringAndIntegerFunction creates an XFunction from a function that takes a string and an integer
 func StringAndIntegerFunction(name string, f func(utils.Environment, types.XString, int) types.XValue) XFunction {
-	return func(env utils.Environment, args ...types.XValue) types.XValue {
-		if len(args) != 2 {
-			return types.NewXErrorf("%s takes two arguments, got %d", strings.ToUpper(name), len(args))
-		}
-
-		// if either argument is an error, return immediately
-		if types.IsError(args[0]) {
-			return args[0]
-		}
-		if types.IsError(args[1]) {
-			return args[1]
-		}
-
+	return ErrorPassThru(ArgCountCheck(name, 2, func(env utils.Environment, args ...types.XValue) types.XValue {
 		num, err := types.ToInteger(args[1])
 		if err != nil {
 			return types.NewXError(err)
 		}
 
 		return f(env, types.ToXString(args[0]), num)
-	}
+	}))
 }
 
 // OneNumberFunction creates an XFunction from a single number function
 func OneNumberFunction(name string, f func(utils.Environment, types.XNumber) types.XValue) XFunction {
-	return func(env utils.Environment, args ...types.XValue) types.XValue {
-		if len(args) != 1 {
-			return types.NewXErrorf("%s takes a single argument, got %d", strings.ToUpper(name), len(args))
-		}
-
-		// if argument is an error, return immediately
-		if types.IsError(args[0]) {
-			return args[0]
-		}
-
+	return ErrorPassThru(ArgCountCheck(name, 1, func(env utils.Environment, args ...types.XValue) types.XValue {
 		num, err := types.ToXNumber(args[0])
 		if err != nil {
 			return types.NewXError(err)
 		}
 
 		return f(env, num)
-	}
+	}))
 }
 
 // TwoNumberFunction creates an XFunction from a function that takes two numbers
 func TwoNumberFunction(name string, f func(utils.Environment, types.XNumber, types.XNumber) types.XValue) XFunction {
-	return func(env utils.Environment, args ...types.XValue) types.XValue {
-		if len(args) != 2 {
-			return types.NewXErrorf("%s takes two arguments, got %d", strings.ToUpper(name), len(args))
-		}
-
-		// if either argument is an error, return immediately
-		if types.IsError(args[0]) {
-			return args[0]
-		}
-		if types.IsError(args[1]) {
-			return args[1]
-		}
-
+	return ErrorPassThru(ArgCountCheck(name, 2, func(env utils.Environment, args ...types.XValue) types.XValue {
 		num1, err := types.ToXNumber(args[0])
 		if err != nil {
 			return types.NewXError(err)
@@ -111,26 +79,17 @@ func TwoNumberFunction(name string, f func(utils.Environment, types.XNumber, typ
 		}
 
 		return f(env, num1, num2)
-	}
+	}))
 }
 
 // OneDateFunction creates an XFunction from a single number function
 func OneDateFunction(name string, f func(utils.Environment, types.XTime) types.XValue) XFunction {
-	return func(env utils.Environment, args ...types.XValue) types.XValue {
-		if len(args) != 1 {
-			return types.NewXErrorf("%s takes a single argument, got %d", strings.ToUpper(name), len(args))
-		}
-
-		// if argument is an error, return immediately
-		if types.IsError(args[0]) {
-			return args[0]
-		}
-
+	return ErrorPassThru(ArgCountCheck(name, 1, func(env utils.Environment, args ...types.XValue) types.XValue {
 		date, err := types.ToXTime(env, args[0])
 		if err != nil {
 			return types.NewXError(err)
 		}
 
 		return f(env, date)
-	}
+	}))
 }
