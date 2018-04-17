5b157a770223e05a2b95b7a78b32cb3b0fd224bb
Rowan Seymour <rowanseymour@gmail.com>
2018-04-17 13:29:14 -0500

    fix typo

--- test/session.go
+++ test/session.go
@@ -305,10 +305,7 @@ type testEnvironment struct {
 
 // creates a new test environment
 func newTestEnvironment() utils.Environment {
-	tz, err := time.LoadLocation("America/Guayaquil")
-	if err != nil {
-		panic("unabel to load location")
-	}
+	tz, _ := time.LoadLocation("America/Guayaquil")
 
 	return &testEnvironment{
 		utils.NewEnvironment(utils.DateFormatYearMonthDay, utils.TimeFormatHourMinute, tz, utils.LanguageList{"eng", "spa"}),
