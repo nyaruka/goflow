66c6b1d3edac703d9a1447275af69d67332b5d3a
Rowan Seymour <rowanseymour@gmail.com>
2018-04-24 12:16:59 -0500

    Remove unused method

--- flows/engine/config.go
+++ flows/engine/config.go
@@ -9,11 +9,6 @@ type config struct {
 	maxWebhookResponseBytes int
 }
 
-// NewConfig returns a new engine configuration
-func NewConfig(maxWebhookResponseBytes int) flows.EngineConfig {
-	return &config{maxWebhookResponseBytes: maxWebhookResponseBytes}
-}
-
 // NewDefaultConfig returns the default engine configuration
 func NewDefaultConfig() flows.EngineConfig {
 	return &config{maxWebhookResponseBytes: 10000}
