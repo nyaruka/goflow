b0eefa0999c0901318784b2fddd7e1de91eaae00
Rowan Seymour <rowanseymour@gmail.com>
2018-05-03 08:56:50 -0500

    Coverage

--- flows/references_test.go
+++ flows/references_test.go
@@ -1,46 +1,48 @@
-package flows
+package flows_test
 
 import (
 	"testing"
 
+	"github.com/nyaruka/goflow/flows"
 	"github.com/nyaruka/goflow/utils"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestReferenceValidation(t *testing.T) {
 	// channel references must always be concrete
-	assert.NoError(t, utils.Validate(&ChannelReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Nexmo"}))
-	assert.EqualError(t, utils.Validate(&ChannelReference{Name: "Nexmo"}), "field 'uuid' is required")
+	assert.NoError(t, utils.Validate(flows.NewChannelReference("61602f3e-f603-4c70-8a8f-c477505bf4bf", "Nexmo")))
+	assert.EqualError(t, utils.Validate(flows.NewChannelReference("", "Nexmo")), "field 'uuid' is required")
 
 	// contact references must always be concrete
-	assert.NoError(t, utils.Validate(&ContactReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Bob"}))
-	assert.EqualError(t, utils.Validate(&ContactReference{Name: "Bob"}), "field 'uuid' is required")
+	assert.NoError(t, utils.Validate(flows.NewContactReference("61602f3e-f603-4c70-8a8f-c477505bf4bf", "Bob")))
+	assert.EqualError(t, utils.Validate(flows.NewContactReference("", "Bob")), "field 'uuid' is required")
 
 	// group references can be concrete or a name match template
-	assert.NoError(t, utils.Validate(&GroupReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Testers"}))
-	assert.NoError(t, utils.Validate(&GroupReference{NameMatch: "@contact.fields.district"}))
+	assert.NoError(t, utils.Validate(flows.NewGroupReference("61602f3e-f603-4c70-8a8f-c477505bf4bf", "Testers")))
+	assert.NoError(t, utils.Validate(flows.NewVariableGroupReference("@contact.fields.district")))
 
 	// but they can't be neither or both of those things
 	assert.EqualError(t,
-		utils.Validate(&GroupReference{}),
+		utils.Validate(&flows.GroupReference{}),
 		"field 'uuid' is mutually exclusive with 'name_match', field 'name_match' is mutually exclusive with 'uuid'",
 	)
 	assert.EqualError(t,
-		utils.Validate(&GroupReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Bob", NameMatch: "@contact.fields.district"}),
+		utils.Validate(&flows.GroupReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Bob", NameMatch: "@contact.fields.district"}),
 		"field 'uuid' is mutually exclusive with 'name_match', field 'name_match' is mutually exclusive with 'uuid'",
 	)
 
 	// label references can be concrete or a name match template
-	assert.NoError(t, utils.Validate(&LabelReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Spam"}))
-	assert.NoError(t, utils.Validate(&LabelReference{NameMatch: "@contact.fields.district"}))
+	assert.NoError(t, utils.Validate(flows.NewLabelReference("61602f3e-f603-4c70-8a8f-c477505bf4bf", "Spam")))
+	assert.NoError(t, utils.Validate(flows.NewVariableLabelReference("@contact.fields.district")))
 
 	// but they can't be neither or both of those things
 	assert.EqualError(t,
-		utils.Validate(&LabelReference{}),
+		utils.Validate(&flows.LabelReference{}),
 		"field 'uuid' is mutually exclusive with 'name_match', field 'name_match' is mutually exclusive with 'uuid'",
 	)
 	assert.EqualError(t,
-		utils.Validate(&LabelReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Spam", NameMatch: "@contact.fields.district"}),
+		utils.Validate(&flows.LabelReference{UUID: "61602f3e-f603-4c70-8a8f-c477505bf4bf", Name: "Spam", NameMatch: "@contact.fields.district"}),
 		"field 'uuid' is mutually exclusive with 'name_match', field 'name_match' is mutually exclusive with 'uuid'",
 	)
 }
