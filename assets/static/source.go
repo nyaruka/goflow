ee37e3e15f78b74f86aeba3cebc52ee874118e3b
Rowan Seymour <rowanseymour@gmail.com>
2018-10-10 12:11:27 -0500

    Switch all tests (except runner_test) to use static assets

--- assets/static/source.go
+++ assets/static/source.go
@@ -2,55 +2,66 @@
 package static
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 
 	"github.com/nyaruka/goflow/assets"
+	"github.com/nyaruka/goflow/assets/static/types"
 	"github.com/nyaruka/goflow/utils"
 )
 
 // StaticSource is an asset source which loads assets from a static JSON file
 type StaticSource struct {
 	s struct {
-		Channels  []assets.Channel           `json:"channels" validate:"omitempty,dive"`
-		Fields    []assets.Field             `json:"fields" validate:"omitempty,dive"`
-		Flows     []assets.Flow              `json:"flows" validate:"omitempty,dive"`
-		Groups    []assets.Group             `json:"groups" validate:"omitempty,dive"`
-		Labels    []assets.Label             `json:"labels" validate:"omitempty,dive"`
-		Locations []assets.LocationHierarchy `json:"locations" validate:"omitempty,dive"`
-		Resthooks []assets.Resthook          `json:"resthooks" validate:"omitempty,dive"`
+		Channels  json.RawMessage   `json:"channels"`
+		Fields    json.RawMessage   `json:"fields"`
+		Flows     []json.RawMessage `json:"flows"`
+		Groups    json.RawMessage   `json:"groups"`
+		Labels    json.RawMessage   `json:"labels"`
+		Locations json.RawMessage   `json:"locations"`
+		Resthooks json.RawMessage   `json:"resthooks"`
 	}
 }
 
-// NewStaticSource creates a new static loaded from the given JSON file
-func NewStaticSource(path string) (*StaticSource, error) {
+// NewStaticSource creates a new static source from the given JSON
+func NewStaticSource(data json.RawMessage) (*StaticSource, error) {
+	s := &StaticSource{}
+	if err := utils.UnmarshalAndValidate(data, &s.s); err != nil {
+		return nil, fmt.Errorf("unable to read assets: %s", err)
+	}
+	return s, nil
+}
+
+// LoadStaticSource loads a new static source from the given JSON file
+func LoadStaticSource(path string) (*StaticSource, error) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("error reading file '%s': %s", path, err)
 	}
-
-	s := &StaticSource{}
-	if err := utils.UnmarshalAndValidate(data, s); err != nil {
-		return nil, fmt.Errorf("unable to read assets: %s", err)
-	}
-	return s, nil
+	return NewStaticSource(data)
 }
 
 var _ assets.AssetSource = (*StaticSource)(nil)
 
 // Channels returns all channel assets
 func (s *StaticSource) Channels() ([]assets.Channel, error) {
-	return s.s.Channels, nil
+	return types.ReadChannels(s.s.Channels)
 }
 
 // Fields returns all field assets
 func (s *StaticSource) Fields() ([]assets.Field, error) {
-	return s.s.Fields, nil
+	return types.ReadFields(s.s.Fields)
 }
 
 // Flow returns the flow asset with the given UUID
 func (s *StaticSource) Flow(uuid assets.FlowUUID) (assets.Flow, error) {
-	for _, flow := range s.s.Flows {
+	for _, rawFlow := range s.s.Flows {
+		// TODO inefficient
+		flow, err := types.ReadFlow(rawFlow)
+		if err != nil {
+			return nil, err
+		}
 		if flow.UUID() == uuid {
 			return flow, nil
 		}
@@ -60,20 +71,20 @@ func (s *StaticSource) Flow(uuid assets.FlowUUID) (assets.Flow, error) {
 
 // Groups returns all group assets
 func (s *StaticSource) Groups() ([]assets.Group, error) {
-	return s.s.Groups, nil
+	return types.ReadGroups(s.s.Groups)
 }
 
 // Labels returns all label assets
 func (s *StaticSource) Labels() ([]assets.Label, error) {
-	return s.s.Labels, nil
+	return types.ReadLabels(s.s.Labels)
 }
 
 // Locations returns all location assets
 func (s *StaticSource) Locations() ([]assets.LocationHierarchy, error) {
-	return s.s.Locations, nil
+	return types.ReadLocationHierarchies(s.s.Locations)
 }
 
 // Resthooks returns all resthook assets
 func (s *StaticSource) Resthooks() ([]assets.Resthook, error) {
-	return s.s.Resthooks, nil
+	return types.ReadResthooks(s.s.Resthooks)
 }
