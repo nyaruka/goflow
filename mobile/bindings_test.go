7d90e4c4975dff8e3642fd6781c63c7b02afa4bd
Rowan Seymour <rowanseymour@gmail.com>
2018-10-23 09:32:56 -0500

    Fix test

--- mobile/bindings_test.go
+++ mobile/bindings_test.go
@@ -11,19 +11,30 @@ import (
 )
 
 func TestMobileBindings(t *testing.T) {
-	// if we try to create assets from invalid JSON
+	// error if we try to create assets from invalid JSON
 	_, err := mobile.NewAssetsSource("{")
 	assert.Error(t, err)
 
-	assetsJSON, err := ioutil.ReadFile("testdata/two_questions_offline.json")
+	// can load a standard assets file
+	assetsJSON, err := ioutil.ReadFile("../test/testdata/flows/two_questions_offline.json")
 	require.NoError(t, err)
 
 	source, err := mobile.NewAssetsSource(string(assetsJSON))
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
+	// and create a new session assets
 	sessionAssets, err := mobile.NewSessionAssets(source)
-	assert.NoError(t, err)
+	require.NoError(t, err)
+
+	environment, err := mobile.NewEnvironment("DD-MM-YYYY", "tt:mm", "Africa/Kigali", "eng", []string{"eng", "fra"})
+	require.NoError(t, err)
+
+	contact := mobile.NewEmptyContact()
+
+	trigger := mobile.NewManualTrigger(environment, contact, "7c3db26f-e12a-48af-9673-e2feefdf8516", "Two Questions")
 
 	session := mobile.NewSession(sessionAssets, "mobile-test")
 
+	err = session.Start(trigger)
+	require.NoError(t, err)
 }
