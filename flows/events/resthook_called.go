d05c8a4fe1a71d97ed3ebac90e822553213088c4
Rowan Seymour <rowanseymour@gmail.com>
2019-02-11 09:18:17 -0500

    Add resthook_called event

---
+++ flows/events/resthook_called.go
@@ -0,0 +1,46 @@
+package events
+
+import (
+	"encoding/json"
+
+	"github.com/nyaruka/goflow/flows"
+)
+
+func init() {
+	RegisterType(TypeResthookCalled, func() flows.Event { return &ResthookCalledEvent{} })
+}
+
+// TypeResthookCalled is the type for our resthook events
+const TypeResthookCalled string = "resthook_called"
+
+// ResthookCalledEvent events are created when a resthook is called. The event contains
+// the payload that will be sent to any subscribers of that resthook. Note that this event is
+// created regardless of whether there any subscriberes for that resthook.
+//
+//   {
+//     "type": "resthook_called",
+//     "created_on": "2006-01-02T15:04:05Z",
+//     "resthook": "success",
+//     "payload": {
+//       "contact:":{
+//         "name":"Bob"
+//       }
+//     }
+//   }
+//
+// @event resthook_called
+type ResthookCalledEvent struct {
+	BaseEvent
+
+	Resthook string          `json:"resthook"`
+	Payload  json.RawMessage `json:"payload"`
+}
+
+// NewResthookCalledEvent returns a new webhook called event
+func NewResthookCalledEvent(resthook string, payload json.RawMessage) *ResthookCalledEvent {
+	return &ResthookCalledEvent{
+		BaseEvent: NewBaseEvent(TypeResthookCalled),
+		Resthook:  resthook,
+		Payload:   payload,
+	}
+}
