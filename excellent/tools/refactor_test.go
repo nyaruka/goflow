7d285262c5941dd4209c6d1d791f86be1feaf09c
Rowan Seymour <rowanseymour@gmail.com>
2019-02-25 16:30:24 -0500

    Test that refactored templats evaluate to the same as the originals

--- excellent/tools/refactor_test.go
+++ excellent/tools/refactor_test.go
@@ -3,10 +3,37 @@ package tools_test
 import (
 	"testing"
 
+	"github.com/nyaruka/goflow/excellent"
 	"github.com/nyaruka/goflow/excellent/tools"
+	"github.com/nyaruka/goflow/excellent/types"
+	"github.com/nyaruka/goflow/utils"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type testXObject struct {
+	bar types.XNumber
+}
+
+func newTestXObject(bar int) *testXObject {
+	return &testXObject{bar: types.NewXNumberFromInt(bar)}
+}
+
+// Describe returns a representation of this type for error messages
+func (v *testXObject) Describe() string { return "test" }
+
+func (v *testXObject) Reduce(env utils.Environment) types.XPrimitive { return v.bar }
+
+func (v *testXObject) Resolve(env utils.Environment, key string) types.XValue {
+	return v.bar
+}
+
+// ToXJSON is called when this type is passed to @(json(...))
+func (v *testXObject) ToXJSON(env utils.Environment) types.XText {
+	return types.ResolveKeys(env, v, "bar").ToXJSON(env)
+}
+
+var _ types.XValue = &testXObject{}
 
 func TestRefactorTemplate(t *testing.T) {
 	testCases := []struct {
@@ -30,15 +57,27 @@ func TestRefactorTemplate(t *testing.T) {
 		{`@(1 / ) @(1+2)`, `@(1 / ) @(1 + 2)`, true},
 	}
 
+	env := utils.NewEnvironmentBuilder().Build()
+	vars := types.NewXMap(map[string]types.XValue{
+		"foo": newTestXObject(123),
+	})
+	topLevels := []string{"foo"}
+
 	for _, tc := range testCases {
-		actual, err := tools.RefactorTemplate(tc.template, []string{"foo"})
+		actual, err := tools.RefactorTemplate(tc.template, topLevels)
+
+		assert.Equal(t, tc.refactored, actual, "refactor mismatch for template: %s", tc.template)
 
 		if tc.hasError {
 			assert.Error(t, err, "expected error for template: %s", tc.template)
 		} else {
 			assert.NoError(t, err, "unexpected error for template: %s, err: %s", tc.template, err)
-		}
 
-		assert.Equal(t, tc.refactored, actual, "refactor mismatch for template: %s", tc.template)
+			// test that the original and the refactored template evaluate equally
+			originalValue, _ := excellent.EvaluateTemplate(env, vars, tc.template, topLevels)
+			refactoredValue, _ := excellent.EvaluateTemplate(env, vars, actual, topLevels)
+
+			assert.Equal(t, originalValue, refactoredValue, "refactoring of template %s gives different value: %s", tc.template, refactoredValue)
+		}
 	}
 }
