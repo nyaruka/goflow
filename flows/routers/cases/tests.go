504144d5f9eb9d4e11e68cde75df440de3a3effc
Rowan Seymour <rowanseymour@gmail.com>
2019-04-30 10:50:39 +0100

    Match number like .5 and 1OO (o's) as 1)

--- flows/routers/cases/tests.go
+++ flows/routers/cases/tests.go
@@ -836,8 +836,10 @@ func hasOnlyPhraseTest(origHays []string, hays []string, pins []string) types.XV
 
 // ParseDecimalFuzzy parses a decimal from a string
 func ParseDecimalFuzzy(val string, format *utils.NumberFormat) (decimal.Decimal, error) {
+	cleaned := strings.TrimSpace(val)
+
 	// remove digit grouping symbol
-	cleaned := strings.Replace(val, format.DigitGroupingSymbol, "", -1)
+	cleaned = strings.Replace(cleaned, format.DigitGroupingSymbol, "", -1)
 
 	// replace non-period decimal symbols
 	cleaned = strings.Replace(cleaned, format.DecimalSymbol, ".", -1)
@@ -849,7 +851,7 @@ type decimalTest func(value decimal.Decimal, test1 decimal.Decimal, test2 decima
 
 func testNumber(env utils.Environment, str types.XText, testNum1 types.XNumber, testNum2 types.XNumber, testFunc decimalTest) types.XValue {
 	// create a number finding regex based on current environment
-	pattern := regexp.MustCompile(fmt.Sprintf(`[-+]?[\pNlO\%s]+(\%s[\pNlO]+)?`, env.NumberFormat().DigitGroupingSymbol, env.NumberFormat().DecimalSymbol))
+	pattern := regexp.MustCompile(fmt.Sprintf(`[-+]?([\pN\%[1]s]+(\%[2]s[\pN]+)?|(\W|^)\%[2]s[\pN]+)`, env.NumberFormat().DigitGroupingSymbol, env.NumberFormat().DecimalSymbol))
 
 	// look for number like things in the input and use the first one that we can actually parse
 	for _, value := range pattern.FindAllString(str.Native(), -1) {
