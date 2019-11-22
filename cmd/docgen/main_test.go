0e1acb5fd8a5ad3033a2ea9a8626cad3a30327ca
Rowan Seymour <rowanseymour@gmail.com>
2019-11-22 17:09:17 -0500

    globals should be a dynamic type in the context which the editor can provide keys to

--- cmd/docgen/main_test.go
+++ cmd/docgen/main_test.go
@@ -41,7 +41,7 @@ func TestGenerateDocs(t *testing.T) {
 	assert.Contains(t, completion, "root")
 
 	types := completion["types"].([]interface{})
-	assert.Equal(t, 12, len(types))
+	assert.Equal(t, 13, len(types))
 
 	root := completion["root"].([]interface{})
 	assert.Equal(t, 11, len(root))
