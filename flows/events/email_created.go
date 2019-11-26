46a8113cbf96af2090d81b093a0a87d074982bbd
Rowan Seymour <rowanseymour@gmail.com>
2019-11-26 12:21:07 -0500

    Put email_created event back as it would be needed to load old sessions

---
+++ flows/events/email_created.go
@@ -0,0 +1,21 @@
+package events
+
+import (
+	"github.com/nyaruka/goflow/flows"
+)
+
+func init() {
+	registerType(TypeEmailCreated, func() flows.Event { return &EmailCreatedEvent{} })
+}
+
+// TypeEmailCreated is our type for the email event
+const TypeEmailCreated string = "email_created"
+
+// EmailCreatedEvent is no longer used but old sessions might include these
+type EmailCreatedEvent struct {
+	baseEvent
+
+	Addresses []string `json:"addresses" validate:"required,min=1"`
+	Subject   string   `json:"subject" validate:"required"`
+	Body      string   `json:"body"`
+}
