a062ca1d99788b6d505ccc4f775dce9f82410a48
Rowan Seymour <rowanseymour@gmail.com>
2020-03-16 10:06:10 -0500

    Fix loading flow assets that are new spec but also have metadata section

--- assets/static/types/flow_test.go
+++ assets/static/types/flow_test.go
@@ -30,4 +30,14 @@ func TestFlow(t *testing.T) {
 	assert.Equal(t, assets.FlowUUID("834ab66a-cc95-4a4f-8a45-2ff9cd2ec4ab"), f.UUID())
 	assert.Equal(t, "Legacy", f.Name())
 	assert.Equal(t, definition, f.Definition())
+
+	// sometimes new flows also have a metadata section
+	definition = json.RawMessage(`{"uuid": "f5263dca-469b-47c2-be4f-845d3a14eedf", "name": "Registration", "nodes": [], "metadata": {"revision": 1}}`)
+	f = &types.Flow{}
+	err = jsonx.Unmarshal(definition, f)
+
+	assert.NoError(t, err)
+	assert.Equal(t, assets.FlowUUID("f5263dca-469b-47c2-be4f-845d3a14eedf"), f.UUID())
+	assert.Equal(t, "Registration", f.Name())
+	assert.Equal(t, definition, f.Definition())
 }
