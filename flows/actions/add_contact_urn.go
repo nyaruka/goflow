fabb55b513a288dd50ad578a49f2def5e9eacf4b
Rowan Seymour <rowanseymour@gmail.com>
2020-07-24 15:36:35 -0500

    Move modifiers package out of actions package

--- flows/actions/add_contact_urn.go
+++ flows/actions/add_contact_urn.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/nyaruka/gocommon/urns"
 	"github.com/nyaruka/goflow/flows"
-	"github.com/nyaruka/goflow/flows/actions/modifiers"
 	"github.com/nyaruka/goflow/flows/events"
+	"github.com/nyaruka/goflow/flows/modifiers"
 
 	"github.com/pkg/errors"
 )
