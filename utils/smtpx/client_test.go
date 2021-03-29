cafc35a4a1a65ff7951025eae79ef79bc1614b56
Rowan Seymour <rowanseymour@gmail.com>
2021-03-29 17:19:46 -0500

    Improve tests for email URL decoding

--- utils/smtpx/client_test.go
+++ utils/smtpx/client_test.go
@@ -20,19 +20,19 @@ func TestNewClientFromURL(t *testing.T) {
 	assert.EqualError(t, err, "missing credentials in connection URL")
 
 	// from and port are optional
-	s, err := NewClientFromURL("smtp://leah:pass123@temba.io")
+	s, err := NewClientFromURL("smtp://leah:pass123%21@temba.io")
 	assert.NoError(t, err)
 	assert.Equal(t, "temba.io", s.host)
 	assert.Equal(t, 25, s.port)
 	assert.Equal(t, "leah", s.username)
-	assert.Equal(t, "pass123", s.password)
+	assert.Equal(t, "pass123!", s.password)
 	assert.Equal(t, "leah@temba.io", s.from)
 
-	s, err = NewClientFromURL("smtp://leah:pass123@temba.io:255?from=updates@temba.io")
+	s, err = NewClientFromURL("smtp://leah%40nyaruka.com:pass123%21@temba.io:255?from=Leah+%3Cupdates%40temba.io%3E")
 	assert.NoError(t, err)
 	assert.Equal(t, "temba.io", s.host)
 	assert.Equal(t, 255, s.port)
-	assert.Equal(t, "leah", s.username)
-	assert.Equal(t, "pass123", s.password)
-	assert.Equal(t, "updates@temba.io", s.from)
+	assert.Equal(t, "leah@nyaruka.com", s.username)
+	assert.Equal(t, "pass123!", s.password)
+	assert.Equal(t, "Leah <updates@temba.io>", s.from)
 }
