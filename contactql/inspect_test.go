659cdb3d3ec5b812db20243bd524cc45027aef4c
Rowan Seymour <rowanseymour@gmail.com>
2021-07-30 11:51:16 -0500

    Simplify parsing contact queries

--- contactql/inspect_test.go
+++ contactql/inspect_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/nyaruka/goflow/assets"
+	"github.com/nyaruka/goflow/assets/static/types"
 	"github.com/nyaruka/goflow/contactql"
 	"github.com/nyaruka/goflow/envs"
 
@@ -12,12 +13,23 @@ import (
 )
 
 func TestInspect(t *testing.T) {
+	env := envs.NewBuilder().Build()
+	resolver := contactql.NewMockResolver(map[string]assets.Field{
+		"age":    types.NewField(assets.FieldUUID("f1b5aea6-6586-41c7-9020-1a6326cc6565"), "age", "Age", assets.FieldTypeNumber),
+		"dob":    types.NewField(assets.FieldUUID("3810a485-3fda-4011-a589-7320c0b8dbef"), "dob", "DOB", assets.FieldTypeDatetime),
+		"gender": types.NewField(assets.FieldUUID("d66a7823-eada-40e5-9a3a-57239d4690bf"), "gender", "Gender", assets.FieldTypeText),
+	}, map[string]assets.Group{
+		"u-reporters": types.NewGroup(assets.GroupUUID("4eeca453-f474-4767-bdd0-434b180223db"), "U-Reporters", ""),
+	})
+
 	tests := []struct {
 		query      string
+		resolver   contactql.Resolver
 		inspection *contactql.Inspection
 	}{
 		{
-			query: "bob",
+			query:    "bob",
+			resolver: resolver,
 			inspection: &contactql.Inspection{
 				Attributes:   []string{"name"},
 				Schemes:      []string{},
@@ -39,7 +51,21 @@ func TestInspect(t *testing.T) {
 			},
 		},
 		{
-			query: "id = 123",
+			query:    "AGE > 18 AND name != \"\" OR twitter = bobby OR tel ~1234",
+			resolver: resolver,
+			inspection: &contactql.Inspection{
+				Attributes: []string{"name"},
+				Schemes:    []string{"tel", "twitter"},
+				Fields: []*assets.FieldReference{
+					assets.NewFieldReference("age", "Age"),
+				},
+				Groups:       []*assets.GroupReference{},
+				AllowAsGroup: true,
+			},
+		},
+		{
+			query:    "id = 123",
+			resolver: resolver,
 			inspection: &contactql.Inspection{
 				Attributes:   []string{"id"},
 				Schemes:      []string{},
@@ -49,7 +75,7 @@ func TestInspect(t *testing.T) {
 			},
 		},
 		{
-			query: "group = U-reporters",
+			query: "group = U-reporters", // group condition parsed without resolver
 			inspection: &contactql.Inspection{
 				Attributes: []string{"group"},
 				Schemes:    []string{},
@@ -60,12 +86,23 @@ func TestInspect(t *testing.T) {
 				AllowAsGroup: false,
 			},
 		},
+		{
+			query:    "group = U-reporters", // group condition parsed with resolver
+			resolver: resolver,
+			inspection: &contactql.Inspection{
+				Attributes: []string{"group"},
+				Schemes:    []string{},
+				Fields:     []*assets.FieldReference{},
+				Groups: []*assets.GroupReference{
+					assets.NewGroupReference("4eeca453-f474-4767-bdd0-434b180223db", "U-Reporters"),
+				},
+				AllowAsGroup: false,
+			},
+		},
 	}
 
-	env := envs.NewBuilder().Build()
-
 	for _, tc := range tests {
-		query, err := contactql.ParseQuery(env, tc.query)
+		query, err := contactql.ParseQuery(env, tc.query, tc.resolver)
 		require.NoError(t, err, "error parsing %s", tc.query)
 
 		assert.Equal(t, tc.inspection, contactql.Inspect(query), "inspect mismatch for query %s", tc.query)
