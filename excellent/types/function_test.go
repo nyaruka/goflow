dea54b6644107990bed359952a9f220d567916f7
Rowan Seymour <rowanseymour@gmail.com>
2021-11-08 14:59:46 -0500

    Coverage

--- excellent/types/function_test.go
+++ excellent/types/function_test.go
@@ -14,6 +14,8 @@ func TestXFunction(t *testing.T) {
 
 	func1 := types.NewXFunction("nill", func(env envs.Environment, args ...types.XValue) types.XValue { return nil })
 	func2 := types.NewXFunction("foo", func(env envs.Environment, args ...types.XValue) types.XValue { return nil })
+	anon1 := types.NewXFunction("", func(env envs.Environment, args ...types.XValue) types.XValue { return nil })
+	anon2 := types.NewXFunction("", func(env envs.Environment, args ...types.XValue) types.XValue { return nil })
 
 	assert.True(t, func1.Truthy())
 	assert.Equal(t, `nill`, func1.Render())
@@ -21,10 +23,20 @@ func TestXFunction(t *testing.T) {
 	assert.Equal(t, `XFunction[nill]`, func1.String())
 	assert.Equal(t, `nill(...)`, func1.Describe())
 
+	assert.True(t, anon1.Truthy())
+	assert.Equal(t, `<anon>`, anon1.Render())
+	assert.Equal(t, `<anon>`, anon1.Format(env))
+	assert.Equal(t, `XFunction[<anon>]`, anon1.String())
+	assert.Equal(t, `<anon>(...)`, anon1.Describe())
+
 	marshaled, err := jsonx.Marshal(func1)
 	assert.NoError(t, err)
 	assert.Equal(t, `null`, string(marshaled))
 
 	assert.True(t, types.Equals(func1, func1))
 	assert.False(t, types.Equals(func1, func2))
+
+	// anonymous functions are never equal
+	assert.False(t, types.Equals(anon1, anon1))
+	assert.False(t, types.Equals(anon1, anon2))
 }
