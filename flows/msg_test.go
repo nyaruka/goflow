25d01c8465a3e99e9144836316cb693fda9a1408
Norbert Kwizera <norkans7@gmail.com>
2024-01-17 13:47:52 +0200

    More coverage

--- flows/msg_test.go
+++ flows/msg_test.go
@@ -156,3 +156,40 @@ func TestBroadcastTranslations(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, flows.BroadcastTranslations{"spa": {Text: "Adios"}}, bt)
 }
+
+func TestMsgTemplating(t *testing.T) {
+	uuids.SetGenerator(uuids.NewSeededGenerator(12345))
+	defer uuids.SetGenerator(uuids.DefaultGenerator)
+
+	templateRef := assets.NewTemplateReference("61602f3e-f603-4c70-8a8f-c477505bf4bf", "Affirmation")
+
+	msgTemplating := flows.NewMsgTemplating(templateRef, []string{"Ryan Lewis", "boy"}, "0162a7f4_dfe4_4c96_be07_854d5dba3b2b")
+
+	assert.Equal(t, templateRef, msgTemplating.Template())
+	assert.Equal(t, "0162a7f4_dfe4_4c96_be07_854d5dba3b2b", msgTemplating.Namespace())
+	assert.Equal(t, map[string][]flows.TemplateParam{"body": {{Type: "text", Value: "Ryan Lewis"}, {Type: "text", Value: "boy"}}}, msgTemplating.Params())
+
+	// test marshaling our msg
+	marshaled, err := jsonx.Marshal(msgTemplating)
+	require.NoError(t, err)
+
+	test.AssertEqualJSON(t, []byte(`{
+		"namespace":"0162a7f4_dfe4_4c96_be07_854d5dba3b2b",
+		"params":{
+		  "body":[
+			{
+				"type": "text",
+				"value": "Ryan Lewis"
+			},
+			{
+				"type": "text",
+				"value": "boy"
+			}
+		  ]
+		},
+		"template":{
+		  "name":"Affirmation",
+		  "uuid":"61602f3e-f603-4c70-8a8f-c477505bf4bf"
+		}
+	  }`), marshaled, "JSON mismatch")
+}
