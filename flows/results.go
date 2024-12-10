e79a2c0d553406b302ecd73eee1cd91cdc3dc8a8
Rowan Seymour <rowanseymour@gmail.com>
2024-12-10 16:59:07 +0000

    Revert "Rework results so they're keyed by name rather than snakified name"
    This reverts commit 9a19efe9ded910dadfbb3f333d23cea1ddcf3111.

--- flows/results.go
+++ flows/results.go
@@ -125,10 +125,11 @@ func (r Results) Clone() Results {
 	return clone
 }
 
-// Save saves a new result in our map. Returns the old result if it existed.
+// Save saves a new result in our map using the snakified name as the key. Returns the old result if it existed.
 func (r Results) Save(result *Result) (*Result, bool) {
-	old := r[result.Name]
-	r[result.Name] = result
+	key := utils.Snakify(result.Name)
+	old := r[key]
+	r[key] = result
 
 	if old == nil || (old.Value != result.Value || old.Category != result.Category) {
 		return old, true
@@ -136,9 +137,9 @@ func (r Results) Save(result *Result) (*Result, bool) {
 	return nil, false
 }
 
-// Get returns the result with the given name
-func (r Results) Get(name string) *Result {
-	return r[name]
+// Get returns the result with the given key
+func (r Results) Get(key string) *Result {
+	return r[key]
 }
 
 // Context returns the properties available in expressions
@@ -146,8 +147,8 @@ func (r Results) Context(env envs.Environment) map[string]types.XValue {
 	entries := make(map[string]types.XValue, len(r)+1)
 	entries["__default__"] = types.NewXText(r.format())
 
-	for name, v := range r {
-		entries[utils.Snakify(name)] = Context(env, v)
+	for k, v := range r {
+		entries[k] = Context(env, v)
 	}
 	return entries
 }
@@ -163,7 +164,6 @@ func (r Results) format() string {
 }
 
 func (r *Results) UnmarshalJSON(data []byte) error {
-	// load map which may be keyed by snakified name or name
 	var m map[string]*Result
 	if err := json.Unmarshal(data, &m); err != nil {
 		return err
@@ -174,7 +174,7 @@ func (r *Results) UnmarshalJSON(data []byte) error {
 	// we enforce result names being at most 64 chars but old sessions may have longer names
 	for _, v := range m {
 		v.Name = strings.TrimSpace(stringsx.Truncate(v.Name, 64))
-		(*r)[v.Name] = v
+		(*r)[utils.Snakify(v.Name)] = v
 	}
 
 	return nil
