da88539722ad7f52034908e00a5aa8e15e09f3bf
Rowan Seymour <rowanseymour@gmail.com>
2026-04-24 09:53:17 -0500

    Add Translate prefix to test LLM service that converts input to leetspeak

--- test/services/llm.go
+++ test/services/llm.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/nyaruka/goflow/flows"
@@ -15,6 +16,18 @@ type LLMService struct{}
 func NewLLM() *LLMService {
 	return &LLMService{}
 }
+
+var leetify = strings.NewReplacer(
+	"a", "4", "A", "4",
+	"b", "8", "B", "8",
+	"e", "3", "E", "3",
+	"g", "9", "G", "9",
+	"i", "1", "I", "1",
+	"l", "1", "L", "1",
+	"o", "0", "O", "0",
+	"s", "5", "S", "5",
+	"t", "7", "T", "7",
+).Replace
 
 func (s *LLMService) Response(ctx context.Context, instructions, input string, maxTokens int) (*flows.LLMResponse, error) {
 	// If input is a JSON array of strings whose first element is an \error or
@@ -39,6 +52,20 @@ func (s *LLMService) Response(ctx context.Context, instructions, input string, m
 	} else if strings.HasPrefix(instructions, "Categorize") { // instructions like "Categorize... Category2, Category3]" will return "Category3"
 		words := strings.Fields(instructions)
 		output = strings.TrimSuffix(words[len(words)-1], "]")
+	} else if strings.HasPrefix(instructions, "Translate") { // "Translate..." leetifies the input; if "JSON" is mentioned, values of a string->string object
+		if strings.Contains(instructions, "JSON") {
+			obj := map[string]string{}
+			if err := json.Unmarshal([]byte(input), &obj); err != nil {
+				return nil, fmt.Errorf("invalid JSON object input: %w", err)
+			}
+			for k, v := range obj {
+				obj[k] = leetify(v)
+			}
+			b, _ := json.Marshal(obj)
+			output = string(b)
+		} else {
+			output = leetify(input)
+		}
 	} else {
 		output = "You asked:\n\n" + instructions + "\n\n" + input
 	}
